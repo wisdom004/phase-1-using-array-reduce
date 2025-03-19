84fd44c37a1dcde5937d3ee5a1883e924154b179
ian <ianwisdom90@gmail.com>
2025-03-18 19:09:24 -0700

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteryReducer = (state = 0, action) => {
+    switch (action.type) {
+      case 'ADD_BATTERIES':
+        return state + action.payload;
+      default:
+        return state;
+    }
+  };
+
+  const totalBatteries = batteryReducer(0, { type: 'ADD_BATTERIES', payload: 31});
+
+console.log(totalBatteries);
